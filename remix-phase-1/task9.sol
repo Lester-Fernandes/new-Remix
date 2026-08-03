ca4eb7b182b4955330e15100b78c678a32de4ce2
Lester-Fernandes <roseferns1825@gmail.com>
2026-08-03 12:33:08 +0530

    init

---
+++ remix-phase-1/task9.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract immutability {
+
+    uint public constant Max = 100;
+
+    /*
+        function change() public {
+            MAX = 200;
+        }
+
+    */
+}
