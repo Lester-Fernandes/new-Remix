eaaffa179f20846a925d41c6287ee4e03ce1f7fa
Lester-Fernandes <roseferns1825@gmail.com>
2026-08-03 11:22:18 +0530

    init

---
+++ remix-phase-1/task4.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MemortVSStorage {
+
+ function getNumber() public pure returns(uint) {
+    uint number = 100;
+
+    return number;
+ }
+
+}
